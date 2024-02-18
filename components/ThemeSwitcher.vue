5a16e5a5a210c1f48b8d71615d835f189291fdb4
Brandon Pitzen <brandon.pitzen@jamf.com>
2024-02-17 21:38:32 -0500

    initial commit

---
+++ components/ThemeSwitcher.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useColorMode, useCycleList } from '@vueuse/core'
+import { watchEffect } from 'vue-demi'
+
+const mode = useColorMode({
+  emitAuto: true,
+})
+
+const { state, next } = useCycleList(['dark', 'light', 'auto'], { initialValue: mode })
+
+watchEffect(() => mode.value = state.value as any)
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger class="inline-block px-4 py-2 text-white bg-blue-600 rounded">
+      Theme
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="state= 'auto'">
+        System
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="state= 'light'">
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="state= 'dark'">
+        Dark
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
+
+<style>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {
+  Menubar,
+} from "@/components/ui/menubar";
+</script>
+
+<template>
+  <Menubar>
+    <div class="logo">Brandon Pitzen</div>
+    <div class="theme-switcher">
+      <ThemeSwitcher />
+    </div>
+  </Menubar>
+  <slot />
+</template>
+
+<style scoped>
+.logo {
+  flex: 1;
+}
+
+.theme-switcher {
+  margin-left: auto;
+}
+</style>
+
